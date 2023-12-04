52fcc278b205d3dd49734f43fb4e4ce8b727e5a9
Gael Hofman <Gieltie@outlook.fr>
2023-12-04 16:00:33 +0100

    images fonctionne

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -2,48 +2,18 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { FaSearch } from 'react-icons/fa';
 import './Weather.css';
-/* import { makeStyles } from '@material-ui/core/styles'; */
-
-/* const useStyles = makeStyles({
-  watchContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '500px',
-    backgroundColor: '#000',
-    borderRadius: '10%',
-    border: '10px solid #444',
-    maxWidth: '400px',
-    margin: '0 auto',
-    padding: '20px',
-  },
-  form: {
-    backgroundColor: '#fff',
-    padding: '10px',
-    borderRadius: '5px',
-    color: '#000',
-    marginBottom: '20px',
-  },
-}); */
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
-  //const [cityImage, setCityImage] = useState(null);
+  const [cityImage, setCityImage] = useState(null);
   const [city, setCity] = useState('');
-  /* const classes = useStyles(); */
 
-/*   const fetchCityImage = async (cityName) => {
+  const fetchCityImage = async (cityName) => {
     const apiUrl = 'Ooak_dDC5hujuD6yKFLycI7F1kJS4dBbwGvWn36DmvE'
     const response = await axios.get(`https://api.unsplash.com/search/photos?query=${cityName}&client_id=${apiUrl}`);
     console.log(response.data.results[0].urls.regular)
     setCityImage(response.data.results[0].urls.regular);
-  }; */
-
-  /* const fetchInseeCode = async (cityName) => {
-    const response = await axios.get(`https://geo.api.gouv.fr/communes?nom=${cityName}&fields=nom,code&format=json&geometry=centre`);
-    return response.data[0].code;
-  }; */
+  };
 
   const fetchWeather = async (cityName) => {
     if (cityName) {
@@ -58,28 +28,33 @@ const Weather = () => {
   const handleSearch = async (event) => {
     event.preventDefault();
     fetchWeather(city);
-    //fetchCityImage(city);
+    fetchCityImage(city);
   };
 
   return (
-    <div className='container'>
+    <div className={`container ${cityImage ? '' : 'gradient'}`} style={cityImage ? { backgroundImage: `url(${cityImage})` } : {}}>
       <form onSubmit={handleSearch} className='top-bar'>
         <input
-          type="text"
+          type='text'
           value={city}
           onChange={(e) => setCity(e.target.value)}
-          placeholder="Nom de la ville"
+          placeholder='Nom de la ville'
           className='input'
         />
-        <button type="submit" className='search-icon'><FaSearch/></button>
+        <button type='submit' className='search-icon'><FaSearch/></button>
       </form>
       {weatherData && (
-        <div>
+        <div className='info-city'>
           <h2>Ville: {weatherData.name}</h2>
           <p>Temperature: {Math.ceil(weatherData.main.temp - 273.15)} °C</p>
-          {/* <img src={cityImage} alt="" className='image'/> */}
         </div>
       )}
+      {/* {weatherData && (
+        <div className='info-city'>
+          <h2>Previsons: {weatherData.name}</h2>
+          <p>Temperaturehot: {Math.ceil(weatherData.main.temp - 273.15)} °C</p>
+        </div>
+      )} */}
     </div>
   );
 };
